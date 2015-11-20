0432aa50f8ec9fcede014da589382cfc85aa50b9
Chris Wayne <cwayne@ubuntu.com>
2015-11-19 19:48:54 -0500

    Done do ../www

--- main.go
+++ main.go
@@ -3,6 +3,6 @@ package main
 import "net/http"
 
 func main() {
-        panic(http.ListenAndServe(":5188", http.FileServer(http.Dir("../www"))))
+        panic(http.ListenAndServe(":5188", http.FileServer(http.Dir("."))))
 }
 
