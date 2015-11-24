ce180ab57e1e781dc3ef11087df6516e988e1e86
cwayne18 <cwayne@ubuntu.com>
2015-11-24 12:20:51 -0500

    Call a script called reboot-factory when /reboot is served

--- main.go
+++ main.go
@@ -1,8 +1,29 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"os/exec"
+)
 
 func main() {
-        panic(http.ListenAndServe(":5188", http.FileServer(http.Dir("."))))
+	http.Handle("/", http.FileServer(http.Dir(".")))
+	http.HandleFunc("/reboot", serveTemplate)
+	http.ListenAndServe(":5188", nil)
 }
 
+func reboot() {
+	cmd := "reboot-factory"
+	fmt.Println("reboot-factory")
+	args := []string{}
+	if err := exec.Command(cmd, args...).Run(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+	}
+}
+
+func serveTemplate(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("PRESSED")
+	reboot()
+	http.FileServer(http.Dir("."))
+}
